76511a49be3632abe2b92560850474298d85a770
Ali <ali19961402@gmail.com>
2023-06-25 20:23:36 +0430

    Responsive-2

--- app.js
+++ app.js
@@ -38,7 +38,10 @@ function myfunc(e) {
         h1.style = "width: 100%";
         h1.innerHTML = `<div class="box-data">
                         <h2 style="color:crimson; style="margin-bottom: 15px;"">${data.location.name}</h2>
-                        <img src="${data.current.condition.icon}" alt="" style="display:block;">  
+                        <img src="${data.current.condition.icon}" alt="" style="width: 100px;
+                            height: 100px;
+                            display: flex;
+                            border-radius: 100%;">  
                         <h3 style="margin-bottom: 15px;">Country : ${data.location.country}</h3>
                         <h3 style="margin-bottom: 15px;">Weather :  ${data.current.condition.text}</h3>
                         <div class="time"  style="margin-bottom: 15px;">
